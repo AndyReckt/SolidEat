2dc9857fd34f436584ed3c243eb4aa96aac749b9
Ghoostyy <thomas.sun@outlook.fr>
2024-05-02 01:59:10 +0200

    remove search bar in navigation

--- frontend/src/app/direction/page.tsx
+++ frontend/src/app/direction/page.tsx
@@ -107,29 +107,11 @@ export default function RestaurantDetailPage() {
                     </button>
                 </Link>
             </div>
-            <div className="items-center justify-center flex-grow flex flex-col">
-                <label className="input input-bordered flex items-center gap-2 w-full max-w-sm mb-10 mt-10">
-                    <input
-                        type="text"
-                        className="w-full"
-                        placeholder="Search"
-                    />
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                        className="w-4 h-4 opacity-70">
-                        <path
-                            fillRule="evenodd"
-                            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                            clipRule="evenodd"
-                        />
-                    </svg>
-                </label>
+            <div className="items-center justify-center flex-grow flex flex-col py-10">
                 <div id="map" style={containerStyle}></div>
                 <div className="card card-side bg-white shadow-xl text-black">
                     <figure>
-                        <Image src="" alt="img" className="max-w-full" />
+                        <Image src="" alt="" className="max-w-full" />
                     </figure>
                     <div className="card-body">
                         <h2 className="card-title">{restaurant!.name}</h2>{" "}
