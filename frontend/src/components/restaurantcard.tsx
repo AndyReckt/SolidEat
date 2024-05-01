b1024a0b7489535e6b364357ce1125172d843a03
Ghoostyy <thomas.sun@outlook.fr>
2024-05-01 16:13:56 +0200

    fix restaurant card scaling

--- frontend/src/components/restaurantcard.tsx
+++ frontend/src/components/restaurantcard.tsx
@@ -14,26 +14,23 @@ export default function RestaurantCard(restaurant: Restaurant) {
     };
 
     return (
-        <div className="card card-side bg-white shadow-xl">
-            <figure>
-                <Image src="" alt="Image" className="max-w-full" />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title">{restaurant.name}</h2>{" "}
-                {/* Récupérer le nom du restaurant */}
-                <p>{restaurant.address}</p>{" "}
-                {/* Récupérer l'adresse du restaurant */}
-                <p>Horaires d'ouverture</p>{" "}
-                {/* Récupérer les horaires d'ouverture */}
-                <p>Nombre de place réservé sur x</p>{" "}
-                {/* Récupérer le nombre de place réservé */}
-                <div className="card-actions justify-end">
-                    <button onClick={reserved} className="btn btn-primary">
-                        Réservez
-                    </button>{" "}
-                    {/* Récupérer le bouton de réservation (a voir) */}
+        <div className="py-3">
+            <div className="card card-side bg-white shadow-xl" onClick={reserved}>
+                <figure>
+                    <Image src="" alt="" className="max-w-full" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">{restaurant.name}</h2>{" "}
+                    {/* Récupérer le nom du restaurant */}
+                    <p>{restaurant.address}</p>{" "}
+                    {/* Récupérer l'adresse du restaurant */}
+                    <p>Horaires d'ouverture</p>{" "}
+                    {/* Récupérer les horaires d'ouverture */}
+                    <p>Nombre de place réservé sur x</p>{" "}
+                    {/* Récupérer le nombre de place réservé */}
                 </div>
             </div>
         </div>
+
     );
 }
